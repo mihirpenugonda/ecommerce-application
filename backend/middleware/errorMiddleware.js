863a3b0769563f5d2d238fc3a3b5889c40488586
mihirpenugonda <mihirpenugonda@gmail.com>
2021-12-15 22:41:39 +0530

    created frontend initial tempate

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -11,6 +11,20 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  if (err.name == "TokenExpiredError") {
+    err = new ErrorHandler("JsonWebToken has Expired", 400);
+
+    return res
+      .cookie("token", null, {
+        expires: new Date(Date.now()),
+      })
+      .status(err.statusCode)
+      .json({
+        success: false,
+        error: err.message,
+      });
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error: err.message,
